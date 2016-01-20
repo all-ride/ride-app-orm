11ec6d9634ce72966aab10eae4535c3f73f1503e
Kaya <kayalion@kayalion.net>
2016-01-20 16:42:53 +0100

    split fetching of options in fetching of entries and formatting these

--- src/ride/service/OrmService.php
+++ src/ride/service/OrmService.php
@@ -38,6 +38,75 @@ class OrmService {
     }
 
     /**
+     * Gets the possible entries for the provided field
+     * @param \ride\library\orm\model\Model $model
+     * @param \ride\library\orm\definition\field\ModelField $field
+     * @param \ride\library\i18n\translator\Translator $translator
+     * @param mixed $entry
+     * @return array
+     */
+    public function getFieldInputEntries(Model $model, ModelField $field, Translator $translator, $entry = null) {
+        if ($field instanceof PropertyField) {
+            return array();
+        }
+
+        $relationModel = $model->getRelationModel($field->getName());
+        $relationMeta = $relationModel->getMeta();
+
+        $query = $relationModel->createQuery($translator->getLocale());
+        $query->setFetchUnlocalized(true);
+
+        // apply the preset condition
+        $condition = $field->getOption('scaffold.form.condition');
+        if ($condition) {
+            if (!$entry) {
+                $entry = $relationModel->createEntry();
+            }
+
+            $variables = array();
+
+            $reflectionHelper = $this->model->getReflectionHelper();
+            $meta = $this->model->getMeta();
+            $properties = $meta->getProperties();
+            $belongsTo = $meta->getBelongsTo();
+
+            foreach ($properties as $name => $propertyField) {
+                $variables[$name] = $reflectionHelper->getProperty($entry, $name);
+            }
+
+            foreach ($belongsTo as $name => $belongsToField) {
+                $value = $reflectionHelper->getProperty($entry, $name);
+                if (!$value) {
+                    continue;
+                }
+
+                if (is_object($value)) {
+                    $variables[$name] = $value->getId();
+                } else {
+                    $variables[$name] = $value;
+                }
+            }
+
+            $query->addConditionWithVariables($condition, $variables);
+        }
+
+        // apply vocabulary on taxonomy terms
+        $vocabulary = $field->getOption('taxonomy.vocabulary');
+        if ($vocabulary && $relationModel->getName() == 'TaxonomyTerm') {
+            $query->addCondition('{vocabulary.slug} = %1%', $vocabulary);
+        }
+
+        // apply the order
+        $orderField = $relationMeta->getOption('order.field');
+        if ($orderField) {
+            $query->addOrderBy('{' . $orderField . '} ' . $relationMeta->getOption('order.direction', 'ASC'));
+        }
+
+        // return the result
+        return $query->query();
+    }
+
+    /**
      * Gets the options for the provided field
      * @param \ride\library\orm\model\Model $model
      * @param \ride\library\orm\definition\field\ModelField $field
@@ -47,53 +116,9 @@ class OrmService {
      */
     public function getFieldInputOptions(Model $model, ModelField $field, Translator $translator, $entry = null) {
         if (!$field instanceof PropertyField) {
-            $relationModel = $model->getRelationModel($field->getName());
+            $entries = $this->getFieldInputEntries($model, $field, $translator, $entry);
 
-            $condition = $field->getOption('scaffold.form.condition');
-            if ($condition) {
-                $query = $relationModel->createQuery($translator->getLocale());
-                $query->setFetchUnlocalized(true);
-
-                if (!$entry) {
-                    $entry = $relationModel->createEntry();
-                }
-
-                $variables = array();
-
-                $reflectionHelper = $this->model->getReflectionHelper();
-                $meta = $this->model->getMeta();
-                $properties = $meta->getProperties();
-                $belongsTo = $meta->getBelongsTo();
-
-                foreach ($properties as $name => $propertyField) {
-                    $variables[$name] = $reflectionHelper->getProperty($entry, $name);
-                }
-
-                foreach ($belongsTo as $name => $belongsToField) {
-                    $value = $reflectionHelper->getProperty($entry, $name);
-                    if (!$value) {
-                        continue;
-                    }
-
-                    if (is_object($value)) {
-                        $variables[$name] = $value->getId();
-                    } else {
-                        $variables[$name] = $value;
-                    }
-                }
-
-                $query->addConditionWithVariables($condition, $variables);
-
-                $relationMeta = $relationModel->getMeta();
-                $orderField = $relationMeta->getOption('order.field');
-                if ($orderField) {
-                    $query->addOrderBy('{' . $orderField . '} ' . $relationMeta->getOption('order.direction', 'ASC'));
-                }
-
-                $inputOptions = $relationModel->getOptionsFromEntries($query->query());
-            } else {
-                $inputOptions = $relationModel->getEntryList($translator->getLocale(), true);
-            }
+            $inputOptions = $model->getRelationModel($field->getName())->getOptionsFromEntries($entries);
         } else {
             $optionMethod = $field->getOption('scaffold.form.options.method');
             if ($optionMethod) {
@@ -101,7 +126,7 @@ class OrmService {
             } else {
                 $inputOptions = $field->getOption('scaffold.form.options.json');
                 if ($inputOptions) {
-                    $inputOptions = json_decode($selectOptions, true);
+                    $inputOptions = json_decode($inputOptions, true);
                     foreach ($inputOptions as $index => $value) {
                         $inputOptions[$index] = $translator->translate($value);
                     }
